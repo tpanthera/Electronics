b5c251c6d3459c227782607b9191b692d20c5103
tpanthera <bisht11390@gmail.com>
2016-08-29 01:35:12 +0530

    Add files via upload

---
+++ Electronics.java
@@ -0,0 +1,13 @@
+package com.example.manish.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Electronics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electronics);
+    }
+}
